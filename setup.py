be88a7ee800f2c7198d72b85d9e58d8519ed5526
michal-walter <waltermichal92@gmail.com>
2023-03-24 17:33:14 +0100

    testing changes on setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='home_budget',
     version='1.0.0',
-    packages=find_packages(),
-    include_package_data=True,
+    # packages=find_packages(),
+    # include_package_data=True,
     install_requires=[
         'flask',
         'sqlalchemy',
